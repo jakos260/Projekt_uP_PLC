9618ec5c31eb85666c34a09ab6f325b462c2f49a
jakos260 <jankost21@gmail.com>
2024-01-24 01:07:03 +0100

    tb setup

--- tests/run_tb.ps1
+++ tests/run_tb.ps1
@@ -7,7 +7,7 @@ param (
 # Remove-Item -Path ./tests/up_tb.vcd
 
 $files = Get-Content $files_to_tb_path
-iverilog -o tests/up_tb.vvp ./tests/processor_tb.v $files
+iverilog -o tests/up_tb.vvp ./tests/up_tb.v $files
 # ./a.v ./alu.v ./counter.v ./id.v ./mux.v ./rf.v ./rom.v ./registers.v
 vvp tests/up_tb.vvp
-gtkwave -f ./tests/up_tb.vcd -a "processor_tb.P.clk_in" -a "processor_tb.P.rst"
+gtkwave -f ./tests/up_tb.vcd -a "up_tb.P.clk_in" -a "up_tb.P.rst"
